d16d8ae5fab5976b0c9c95bb986bc587d9fb9e8c
Ricardo chies <ricardochies5@gmail.com>
2021-10-27 20:05:52 -0300

    Troca de cores aplicada

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 var button = document.getElementById('button_color')
+var input = document.getElementById('input_color')
+var span = document.getElementById('text_color')
 
 function quandoClicar() {
-    alert("Button clicked")
+    //alert(input.value)
+    var cor = input.value
+
+    document.body.style.backgroundColor = cor
+    span.textContent = cor
 }
+
+// FORMAS PARA ESCREVER UMA COR
+// Cor em inglês: red, green, blue, yellow
+// Hexadecimal: #8B0000, #FFFFFF
+// RGB: rgb (139,0,0)
 
 button.addEventListener('click', quandoClicar)
