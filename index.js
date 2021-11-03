65541e5006a34048068402ac54bba6f7ebc81d65
Ricardo chies <ricardochies5@gmail.com>
2021-11-03 20:56:20 -0300

    Adição cores randomizadas

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-const listaCores = ['blue, DeepSkyBlue, Cyan, Aquamarine, Lime, GreenYellow, Magenta, DeepPink, Crimson, Red, OrangeRed']
+const listaCores = ['Blue', 'DeepSkyBlue', 'Cyan', 'Aquamarine', 'Lime', 'GreenYellow', 'Magenta', 'DeepPink', 'Crimson', 'Red', 'OrangeRed']
 
 var button = document.getElementById('button_color')
 var input = document.getElementById('input_color')
 var span = document.getElementById('text_color')
 
+function numeroRandomico() {
+    return Math.floor(Math.random() * listaCores.length)
+}
+
 function quandoClicar() {
-    var cor = input.value
+    var cor
 
-    if(input.value) {
+    if (input.value) {
+        cor = input.value
         // Tiver algo escreito
-
     } else {
         // Não tiver nada escrito
+        var rand = numeroRandomico()
+        cor = listaCores[rand]
     }
 
     document.body.style.backgroundColor = cor
     span.textContent = cor
-}
 
+}
 // FORMAS PARA ESCREVER UMA COR
 // Cor em inglês: red, green, blue, yellow
 // Hexadecimal: #8B0000, #FFFFFF
