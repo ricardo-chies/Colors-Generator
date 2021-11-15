face4a7e2e1b62bebb80951cd39f2fbc08fe02bf
Ricardo chies <ricardochies5@gmail.com>
2021-11-15 16:41:39 -0300

    Criação página hexadecimal

--- hexa.js
+++ hexa.js
@@ -1,15 +1,21 @@
-const listaHexa = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+var listaHexa = ['A', 'B', 'C', 'D', 'E', 'F', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 var button = document.getElementById('button_color')
 var span = document.getElementById('text_color')
 
 function numeroRandomico() {
-    return Math.floor(Math.random() * listaHexa.length)
+    return '#' + Math.floor(Math.random() * listaHexa.length).toString(16)
 }
 
 function quandoClicar() {
-    document.body.style.backgroundColor = cor
-    span.textContent = cor
+    var rand = numeroRandomico()
+
+    for (var i = 0; i < 5; i++) {
+        rand += listaHexa[Math.floor(Math.random() * 16)]
+    }
+
+    document.body.style.backgroundColor = rand
+    span.textContent = rand
 }
 
 button.addEventListener('click', quandoClicar)
