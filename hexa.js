fe0cca5039ef59a3e97ab7cf60b1347f2033a9ae
Ricardo chies <ricardochies5@gmail.com>
2021-11-15 18:08:44 -0300

    Revert "Criação página hexadecimal"
    This reverts commit face4a7e2e1b62bebb80951cd39f2fbc08fe02bf.

--- hexa.js
+++ hexa.js
@@ -1,21 +1,15 @@
-var listaHexa = ['A', 'B', 'C', 'D', 'E', 'F', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+const listaHexa = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
 
 var button = document.getElementById('button_color')
 var span = document.getElementById('text_color')
 
 function numeroRandomico() {
-    return '#' + Math.floor(Math.random() * listaHexa.length).toString(16)
+    return Math.floor(Math.random() * listaHexa.length)
 }
 
 function quandoClicar() {
-    var rand = numeroRandomico()
-
-    for (var i = 0; i < 5; i++) {
-        rand += listaHexa[Math.floor(Math.random() * 16)]
-    }
-
-    document.body.style.backgroundColor = rand
-    span.textContent = rand
+    document.body.style.backgroundColor = cor
+    span.textContent = cor
 }
 
 button.addEventListener('click', quandoClicar)
