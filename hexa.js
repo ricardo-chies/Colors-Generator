4694dcd5d12f8df9108135d0d8079b7c85e8c2af
Ricardo chies <ricardochies5@gmail.com>
2021-11-16 20:21:34 -0300

    Finalização página Hexadecimal

--- hexa.js
+++ hexa.js
@@ -4,12 +4,18 @@ var button = document.getElementById('button_color')
 var span = document.getElementById('text_color')
 
 function numeroRandomico() {
-    return Math.floor(Math.random() * listaHexa.length)
+    return '#' + Math.floor(Math.random() * listaHexa.length).toString(16)
 }
 
 function quandoClicar() {
-    document.body.style.backgroundColor = cor
-    span.textContent = cor
+    var rand = numeroRandomico()
+
+    for (var i = 0; i < 5; i++) {
+        rand += listaHexa[Math.floor(Math.random() * 16)]
+    }
+
+    document.body.style.backgroundColor = rand
+    span.textContent = rand
 }
 
 button.addEventListener('click', quandoClicar)
